6d428d4c955fc7622326e895fdaae6c21a677aaa
Geethma Dias <geethmadias2002@gmail.com>
2026-02-23 18:32:41 +0530

    promises

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("AF Labsheet 4 is completed.");
+
+const myPromise = new Promise((resolve, reject) => { 
+  if (condition) { 
+    resolve('Success!'); 
+  } else { 
+    reject('Failure!'); 
+  } 
+}); 
+ 
+myPromise.then((result) => { 
+  console.log(result); 
+}).catch((error) => { 
+  console.log(error); 
+}); 
