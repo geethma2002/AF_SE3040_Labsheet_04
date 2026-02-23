5c3db35a35d4fdd37752138009d175abd7281780
Geethma Dias <geethmadias2002@gmail.com>
2026-02-23 18:24:08 +0530

    creating a web server

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("AF Labsheet 4 is completed.");
+
+const http = require('http'); 
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(8080); 
