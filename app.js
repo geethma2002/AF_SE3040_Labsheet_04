1d4a99fed765278dbb6ad61dac05c0bb0c79b440
Geethma Dias <geethmadias2002@gmail.com>
2026-02-23 18:20:54 +0530

    WriteToFile

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("AF Labsheet 4 is completed.");
+
+const fs = require('fs'); 
+fs.writeFile('file.txt', 'Hello World!', function (err) { 
+  if (err) throw err; 
+  console.log('File saved!'); 
+});
