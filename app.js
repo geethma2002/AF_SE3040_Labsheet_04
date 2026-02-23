6f811e3c63e224c473f22ab0f307557903ac2af9
Geethma Dias <geethmadias2002@gmail.com>
2026-02-23 18:34:44 +0530

    Async/Awaits

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("AF Labsheet 4 is completed.");
+
+async function myFunction() { 
+  try { 
+    const result = await myPromise; 
+    console.log(result); 
+  } catch (error) { 
+    console.log(error); 
+  } 
+} 
+ 
+myFunction();
